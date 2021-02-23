f51d9dd7bb4aff8111d16be8f51871b9aa665989
Hugo-Chiang <statesman1991@gmail.com>
2021-02-24 01:49:38 +0800

    testing

--- api/PDO.php
+++ api/PDO.php
@@ -6,8 +6,9 @@ $db_pass = getenv('password');
 $db_select = getenv('database');
 
 $dsn = "mysql:host=" . $db_host.";dbname=" . $db_select;
+$opts_values = array(PDO::ATTR_PERSISTENT=>true,PDO::ATTR_ERRMODE=>2,PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');
 
-$pdo = new PDO($dsn, $db_user, $db_pass);
+$pdo = new PDO($dsn, $db_user, $db_pass, $opts_values);
 
 return $pdo;
 
