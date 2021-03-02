6700031d93927d8bb9cdb6b03b827b0533fffb4b
Hugo-Chiang <statesman1991@gmail.com>
2021-03-02 18:28:11 +0800

    基礎整理與設置本地端

--- Lib/PDO.php
+++ Lib/PDO.php
@@ -1,9 +1,14 @@
 <?php
 
-$db_host = getenv('host');
-$db_user = getenv('username');
-$db_pass = getenv('password');
-$db_select = getenv('database');
+// $db_host = getenv('host');
+// $db_user = getenv('username');
+// $db_pass = getenv('password');
+// $db_select = getenv('database');
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'SQL1933';
+$db_select = 'heroku_483d8bf99e42fe3';
 
 $dsn = "mysql:host=" . $db_host.";dbname=" . $db_select;
 $opts_values = array(PDO::ATTR_PERSISTENT=>true,PDO::ATTR_ERRMODE=>2,PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');
--- api/Checkout.php
+++ api/Checkout.php
@@ -5,13 +5,17 @@ header("Content-Type:text/html; charset=utf-8");
 // header("Access-Control-Allow-Origin: https://side-projects-01-kapitan.herokuapp.com");
 // header('Access-Control-Allow-Methods: GET');
 
-include("../Lib/PDO.php");
+$_POST = json_decode(file_get_contents("php://input"),true);
 
-$sql = "SELECT * FROM PROJECTS";
 
-$result = $pdo->query($sql);
-$data = $result->fetchAll();
 
-print json_encode($data);
+// include("PDO.php");
+
+// $sql = "SELECT * FROM PROJECTS";
+
+// $result = $pdo->query($sql);
+// $data = $result->fetchAll();
+
+// print json_encode($data);
 
 ?>
