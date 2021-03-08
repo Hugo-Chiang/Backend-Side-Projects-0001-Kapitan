cad0b136925aa28df037f7424c8630d6046d0fc1
Hugo-Chiang <statesman1991@gmail.com>
2021-03-09 02:17:22 +0800

    更正登入驗證機制為session

--- api/AdminSignIn.php
+++ api/AdminSignIn.php
@@ -41,36 +41,34 @@ $statement_query_admin_identifier = $pdo->prepare($sql_query_admin_identifier);
 $statement_query_admin_identifier->bindParam(1, $admin_id);
 $statement_query_admin_identifier->execute();
 
-$query_result = $statement_query_admin_identifier->fetch(PDO::FETCH_ASSOC);
+$admin_identifier = $statement_query_admin_identifier->fetch(PDO::FETCH_ASSOC);
 
-// 產生新的登入用 token
-$basic_token = (function () {
-    $resource = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $garbled = '';
+// 執行：選出管理員登入所用的密鑰，以利建立登入驗證用的 session
+$para = 'admin';
+$sql_query_secret_key = 'SELECT SECRET_KEY_VALUE FROM secret_keys WHERE SECRET_KEY_USAGE = ?';
+$statement_query_secret_key = $pdo->prepare($sql_query_secret_key);
+$statement_query_secret_key->bindParam(1, $para);
+$statement_query_secret_key->execute();
 
-    for ($i = 0; $i < 20; $i++) {
-        $garbled .= $resource[rand(0, 20 - 1)];
-    }
+$admin_secret_key = $statement_query_secret_key->fetch(PDO::FETCH_ASSOC);
 
-    return $garbled;
-})();
+// 產生 session
+$session = hash('sha256', $admin_id . $admin_identifier['ADMIN_IDENTIFIER'] . $admin_secret_key['SECRET_KEY_VALUE']);
 
-$new_token = str_shuffle($basic_token . $query_result['ADMIN_IDENTIFIER']);
-
-// 執行：將新 token 寫入資料庫，並連帶產生到期日
+// 執行：將新 session 寫入資料庫，並連帶產生到期日
 $time_now = time();
-$exp_time = $time_now + (60 * 60 * 24 * 7);
+$exp_time = $time_now + (60 * 60 * 24 * 3);
 $exp_time = date("Y-m-d H:i:s", $exp_time);
 
-$sql_update_new_token = 'UPDATE admin SET ADMIN_TOKEN = ?, ADMIN_SIGNIN_TIMEOUT= ? WHERE ADMIN_ID = ?';
-$statement_update_new_token = $pdo->prepare($sql_update_new_token);
-$statement_update_new_token->bindParam(1, $new_token);
-$statement_update_new_token->bindParam(2, $exp_time);
-$statement_update_new_token->bindParam(3, $admin_id);
-$statement_update_new_token->execute();
+$sql_update_new_session = 'UPDATE admin SET ADMIN_SESSION = ?, ADMIN_SIGNIN_TIMEOUT= ? WHERE ADMIN_ID = ?';
+$statement_update_new_session = $pdo->prepare($sql_update_new_session);
+$statement_update_new_session->bindParam(1, $session);
+$statement_update_new_session->bindParam(2, $exp_time);
+$statement_update_new_session->bindParam(3, $admin_id);
+$statement_update_new_session->execute();
 
-// 執行：將新 token 與到期日回傳予前端，以利進行登入驗證
-$sql_query_admin_signedin_data = "SELECT ADMIN_TOKEN, ADMIN_SIGNIN_TIMEOUT FROM admin WHERE ADMIN_ID = ?";
+// 執行：將 session 與到期日回傳予前端，以利進行登入驗證
+$sql_query_admin_signedin_data = "SELECT ADMIN_SESSION, ADMIN_SIGNIN_TIMEOUT FROM admin WHERE ADMIN_ID = ?";
 $statement_query_admin_signedin_data = $pdo->prepare($sql_query_admin_signedin_data);
 $statement_query_admin_signedin_data->bindParam(1, $admin_id);
 $statement_query_admin_signedin_data->execute();
@@ -79,7 +77,7 @@ $query_result = $statement_query_admin_signedin_data->fetch(PDO::FETCH_ASSOC);
 
 $return_obj = (object)[
     'singInStatus' => true,
-    'token' => $query_result['ADMIN_TOKEN'],
+    'session' => $query_result['ADMIN_SESSION'],
     'expDate' => $query_result['ADMIN_SIGNIN_TIMEOUT']
 ];
 
--- api/AdminSignInAuthentication.php
+++ api/AdminSignInAuthentication.php
@@ -10,20 +10,20 @@ header("Content-Type:text/html; charset=utf-8");
 include("../Lib/PDO.php");
 
 // 接收前端 JSON 字串資料並解析
-$token_string = file_get_contents("php://input");
+$session_string = file_get_contents("php://input");
 
-// 執行：查詢現有登入 token 是否存在（有效），並找出持有人是誰
-$sql_query_admin_token = 'SELECT ADMIN_ID FROM admin WHERE ADMIN_TOKEN = ?';
-$statement_query_admin_token = $pdo->prepare($sql_query_admin_token);
-$statement_query_admin_token->bindParam(1, $token_string);
-$statement_query_admin_token->execute();
+// 執行：查詢現有登入 session 是否存在（有效），並找出持有人是誰
+$sql_query_admin_session = 'SELECT ADMIN_ID FROM admin WHERE ADMIN_SESSION = ?';
+$statement_query_admin_session = $pdo->prepare($sql_query_admin_session);
+$statement_query_admin_session->bindParam(1, $session_string);
+$statement_query_admin_session->execute();
 
-$query_result = $statement_query_admin_token->fetch(PDO::FETCH_ASSOC);
+$query_result = $statement_query_admin_session->fetch(PDO::FETCH_ASSOC);
 
-// 若　token　無效，向前端返回錯誤訊息，並中止程式碼執行
+// 若　session　無效，向前端返回錯誤訊息，並中止程式碼執行
 if ($query_result == null) {
     $return_obj = (object)[
-        'tokenCheck' => false,
+        'sessionCheck' => false,
         'message' => '無效的登入標記！'
     ];
 
@@ -37,16 +37,16 @@ $statement_update_signin_authn = $pdo->prepare($sql_update_signin_auth);
 $statement_update_signin_authn->bindParam(1, $query_result['ADMIN_ID']);
 $statement_update_signin_authn->execute();
 
-// 執行：將 token 檢查成功的訊息回傳前端（准許前進），並親切地打聲招呼
-$sql_query_admin_signedin_data = "SELECT ADMIN_NAME FROM admin WHERE ADMIN_TOKEN = ?";
+// 執行：將 session 檢查成功的訊息回傳前端（准許前進），並親切地打聲招呼
+$sql_query_admin_signedin_data = "SELECT ADMIN_NAME FROM admin WHERE ADMIN_SESSION = ?";
 $statement_query_admin_signedin_data = $pdo->prepare($sql_query_admin_signedin_data);
-$statement_query_admin_signedin_data->bindParam(1, $token_string);
+$statement_query_admin_signedin_data->bindParam(1, $session_string);
 $statement_query_admin_signedin_data->execute();
 
 $query_result = $statement_query_admin_signedin_data->fetch(PDO::FETCH_ASSOC);
 
 $return_obj = (object)[
-    'tokenCheck' => true,
+    'sessionCheck' => true,
     'message' => '歡迎回來，' . $query_result['ADMIN_NAME'],
 ];
 
