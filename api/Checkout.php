72295af6af76e7339c89b27b3eaa8f52b66fcff9
Hugo-Chiang <statesman1991@gmail.com>
2021-03-10 14:30:09 +0800

    修正訂單php

--- api/Checkout.php
+++ api/Checkout.php
@@ -20,30 +20,11 @@ $member_id = $json_data->memberID;
 $orderer_contact_info_arr = $json_data->ordererContactInfo;
 $order_details_arr = $json_data->orderDetails;
 
-// 前置語法區域開始
-
-// 資料庫查詢：查詢已經預約（且結帳）的專案，以利即時反饋售罄與否
-$sql_query_booking =
-    "SELECT * FROM booking WHERE BOOKING_DATE = ? && FK_PROJECT_ID_for_BK = ?";
-// 資料庫寫入：綁定已登入的會員帳號，寫入訂單表格（orders）
-$sql_insert_order =
-    "INSERT INTO orders(ORDER_ID, ORDER_DATE, ORDER_TOTAL_CONSUMPTION, ORDER_TOTAL_DISCOUNT, FK_MEMBER_ID_for_OD) 
-VALUES (?, NOW(),'20000','0', ?)";
-// 資料庫寫入：綁定結帳內容，寫入訂單細項表格（order_details）
-$sql_instert_order_detail =
-    "INSERT INTO order_details
-(ORDER_DETAIL_ID, ORDER_DETAIL_AMOUNT, ORDER_DETAIL_MC_NAME, ORDER_DETAIL_MC_PHONE, ORDER_DETAIL_MC_EMAIL, ORDER_DETAIL_EC_NAME, ORDER_DETAIL_EC_PHONE, ORDER_DETAIL_EC_EMAIL, FK_ORDER_ID_for_ODD) 
-VALUES (?, '20000', ?, ?, ?, ?, ?, ?, ?)";
-// 資料庫寫入：綁定結帳內容，寫入預約表格（booking）
-$sql_insert_booking =
-    "INSERT INTO booking(BOOKING_ID, BOOKING_DATE, FK_PROJECT_ID_for_BK, FK_ORDER_DETAIL_ID_for_BK) 
-VALUES (?, ?, ?, ?)";
-
-// 前置語法區域結束
-
-
 // 執行：查詢結帳內容是否包含已預約的內容（購物車階段被人截胡）
 $alerady_booking_arr = [];
+
+$sql_query_booking =
+    "SELECT * FROM booking WHERE BOOKING_DATE = ? && FK_PROJECT_ID_for_BK = ?";
 
 for ($i = 0; $i < count($order_details_arr); $i++) {
     $statement_query_booking = $pdo->prepare($sql_query_booking);
@@ -58,6 +39,7 @@ for ($i = 0; $i < count($order_details_arr); $i++) {
     }
 }
 
+
 if (count($alerady_booking_arr) > 0) {
 
     // 向前端回傳結果：若干方案已有預約
@@ -69,18 +51,36 @@ if (count($alerady_booking_arr) > 0) {
 
     print json_encode($return_obj);
 } else {
-    // 執行：判斷並寫入最新訂單編號
 
+    // 執行：判斷並寫入最新訂單編號
     $insert_order_id = insert_max_id($pdo, 'orders');
+
+    $sql_insert_order = "INSERT INTO 
+    orders(ORDER_ID, ORDER_DATE, ORDER_TOTAL_CONSUMPTION, ORDER_TOTAL_DISCOUNT, 
+    ORDER_MC_NAME, ORDER_MC_PHONE, ORDER_MC_EMAIL, 
+    ORDER_EC_NAME, ORDER_EC_PHONE, ORDER_EC_EMAIL, FK_MEMBER_ID_for_OD) 
+    VALUES (?, NOW(),'20000','0', ?, ?, ?, ?, ?, ?, ?)";
 
     // 執行：訂單寫入 orders 表格
     $statement_insert_order = $pdo->prepare($sql_insert_order);
     $statement_insert_order->bindParam(1, $insert_order_id);
-    $statement_insert_order->bindParam(2, $member_id);
+    $statement_insert_order->bindParam(2, $orderer_contact_info_arr->MCname);
+    $statement_insert_order->bindParam(3, $orderer_contact_info_arr->MCphone);
+    $statement_insert_order->bindParam(4, $orderer_contact_info_arr->MCemail);
+    $statement_insert_order->bindParam(5, $orderer_contact_info_arr->ECname);
+    $statement_insert_order->bindParam(6, $orderer_contact_info_arr->ECphone);
+    $statement_insert_order->bindParam(7, $orderer_contact_info_arr->ECemail);
+    $statement_insert_order->bindParam(8, $member_id);
     $statement_insert_order->execute();
 
     // 執行：訂單細項寫入 order_details 表格，同時預約紀錄寫入 booking 表格
     for ($i = 0; $i < count($order_details_arr); $i++) {
+
+        $sql_instert_order_detail = "INSERT INTO order_details 
+        (ORDER_DETAIL_ID, ORDER_DETAIL_AMOUNT, ORDER_DETAIL_MC_NAME, ORDER_DETAIL_MC_PHONE, ORDER_DETAIL_MC_EMAIL, 
+        ORDER_DETAIL_EC_NAME, ORDER_DETAIL_EC_PHONE, ORDER_DETAIL_EC_EMAIL, FK_ORDER_ID_for_ODD) 
+        VALUES (?, '20000', ?, ?, ?, ?, ?, ?, ?)";
+
         $insert_order_detail_id = insert_max_id($pdo, 'order_details');
 
         $statement_instert_order_detail = $pdo->prepare($sql_instert_order_detail);
@@ -93,6 +93,10 @@ if (count($alerady_booking_arr) > 0) {
         $statement_instert_order_detail->bindParam(7, $order_details_arr[$i]->ECemail);
         $statement_instert_order_detail->bindParam(8, $insert_order_id);
         $statement_instert_order_detail->execute();
+
+        $sql_insert_booking = "INSERT INTO 
+        booking(BOOKING_ID, BOOKING_DATE, FK_PROJECT_ID_for_BK, FK_ORDER_DETAIL_ID_for_BK) 
+        VALUES (?, ?, ?, ?)";
 
         $insert_booking_id = insert_max_id($pdo, 'booking');
 
