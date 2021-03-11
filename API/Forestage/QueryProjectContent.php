ff7881ccd65f2caf68cee32ab45f7388c6810d8a
Hugo-Chiang <statesman1991@gmail.com>
2021-03-12 01:04:59 +0800

    完成產品內容查詢php

---
+++ API/Forestage/QueryProjectContent.php
@@ -0,0 +1,36 @@
+<?php
+
+// 設定 CORS 權限
+header("Access-Control-Allow-Origin: *");
+header("Content-Type:text/html; charset=utf-8");
+// header("Access-Control-Allow-Origin: https://side-projects-01-kapitan.herokuapp.com");
+// header('Access-Control-Allow-Methods: GET');
+
+// 導入 PDO 以安全連線資練庫
+include("../../Lib/PDO.php");
+
+$project_id = file_get_contents("php://input");
+
+// 執行：根據方案 ID 回傳該方案的所有內容
+$sql_query_project_content = "SELECT * FROM projects WHERE PROJECT_ID = ?";
+$statement_query_project_content = $pdo->prepare($sql_query_project_content);
+$statement_query_project_content->bindParam(1, $project_id);
+$statement_query_project_content->execute();
+
+$query_project_content_result = $statement_query_project_content->fetch(PDO::FETCH_ASSOC);
+
+// 執行：根據方案 ID 回傳該方案的所有預約狀態
+$sql_query_project_booking = "SELECT * FROM booking WHERE FK_ORDER_DETAIL_ID_for_BK = ?";
+$statement_query_project_booking = $pdo->prepare($sql_query_project_booking);
+$statement_query_project_booking->bindParam(1, $project_id);
+$statement_query_project_booking->execute();
+
+$query_query_project_booking_result = $statement_query_project_booking->fetchAll(PDO::FETCH_ASSOC);
+
+// 向前端回傳內容
+$return_obj = (object)[
+    'projectContent' => $query_project_content_result,
+    'projectBooking' => $query_query_project_booking_result,
+];
+
+print json_encode($return_obj);
