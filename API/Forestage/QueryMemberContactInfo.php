67c998de3d09543a1ac2e2826b39765179514cf8
Hugo-Chiang <statesman1991@gmail.com>
2021-03-16 19:32:35 +0800

    hotfix

--- API/Forestage/QueryMemberContactInfo.php
+++ API/Forestage/QueryMemberContactInfo.php
@@ -2,7 +2,7 @@
 
 // header("Access-Control-Allow-Origin: *");
 // header("Content-Type:text/html; charset=utf-8");
-header("Access-Control-Allow-Origin: https://side-projects-01-kapitan.herokuapp.com");
+header("Access-Control-Allow-Origin: https://fe-sp-0001-kapitan.herokuapp.com");
 header('Access-Control-Allow-Methods: GET');
 
 // 導入 PDO 以安全連線資練庫
