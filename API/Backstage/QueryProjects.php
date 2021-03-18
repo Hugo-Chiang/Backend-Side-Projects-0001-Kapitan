321c88d64e66b90debbe4cbc758b4d3a0766fc0a
Hugo-Chiang <statesman1991@gmail.com>
2021-03-18 16:51:08 +0800

    fix

--- API/Backstage/QueryProjects.php
+++ API/Backstage/QueryProjects.php
@@ -14,8 +14,12 @@ $json_string = file_get_contents("php://input");
 $json_data = json_decode($json_string);
 
 foreach ($json_data as $properity => $value) {
-    if ($properity != 'projectStatus') $json_data->$properity = '%' . $value . '%';
+    if ($properity != 'projectPriceBeyond' && $value != '%%') {
+        $json_data->$properity = '%' . $value . '%';
+    }
 }
+
+// print json_encode($json_data);
 
 // 執行：
 $sql_query_projects = "SELECT * FROM
@@ -24,12 +28,16 @@ $sql_query_projects = "SELECT * FROM
 JOIN departure_location as dl ON 
 pj.FK_LOCATION_ID_for_PJ = dl.LOCATION_ID) as t1 
 JOIN category as cg ON t1.FK_CATEGORY_ID_for_PJ = cg.CATEGORY_ID) as t2 
-WHERE t2.PROJECT_ID LIKE ? && t2.PROJECT_NAME LIKE ? && t2.PROJECT_STATUS LIKE ?";
+WHERE t2.PROJECT_ID LIKE ? && t2.PROJECT_NAME LIKE ? && t2.PROJECT_STATUS LIKE ? 
+&& t2.CATEGORY_NAME LIKE ? && t2.LOCATION_NAME LIKE ? && t2.PROJECT_ORIGINAL_PRICE_PER_PERSON <= ?";
 
 $statement_query_projects = $pdo->prepare($sql_query_projects);
 $statement_query_projects->bindParam(1, $json_data->projectID);
 $statement_query_projects->bindParam(2, $json_data->projectName);
 $statement_query_projects->bindParam(3, $json_data->projectStatus);
+$statement_query_projects->bindParam(4, $json_data->projectCategory);
+$statement_query_projects->bindParam(5, $json_data->projectDepartureLocation);
+$statement_query_projects->bindParam(6, $json_data->projectPriceBeyond);
 $statement_query_projects->execute();
 
 $query_result = $statement_query_projects->fetchAll(PDO::FETCH_ASSOC);
