34ca66654a5c869db5951061ba7a593789d7501f
Hugo-Chiang <statesman1991@gmail.com>
2021-04-01 20:34:39 +0800

    fix

--- API/Forestage/QueryProjectsList.php
+++ API/Forestage/QueryProjectsList.php
@@ -11,21 +11,19 @@ include("../../Lib/PDO.php");
 $json_string = file_get_contents("php://input");
 $json_data = json_decode($json_string);
 
-// 判斷前端監看的分類是否為全空，全空則等同方案全選，反之則根據所選分類 ID 進行查詢
-if (count($json_data) == 0) {
-    // 執行：
-    $category_id_str = "%%";
-    $sql_query_projects_list = "SELECT 
-    PROJECT_ID, PROJECT_NAME, PROJECT_AVATAR_URL, PROJECT_SUMMARY, PROJECT_ORIGINAL_PRICE_PER_PERSON, PROJECT_MIN_NUM_OF_PEOPLE, 
-    FK_CATEGORY_ID_for_PJ FROM projects WHERE FK_CATEGORY_ID_for_PJ LIKE ? && PROJECT_STATUS > 0 && PROJECT_VISIBLE_ON_WEB != 0";
-    $statement_query_projects_list = $pdo->prepare($sql_query_projects_list);
-    $statement_query_projects_list->bindParam(1, $category_id_str);
-    $statement_query_projects_list->execute();
+$selected_categories = $json_data->selectedCategories;
+$budget = $json_data->budget;
+$sort = $json_data->sort;
+
+// 判斷：地點分類是否為全空，全空則回傳空陣列，反之則根據所選分類 ID 進行查詢
+if (count($selected_categories) == 0) {
+
+    $result_query_projects_list = [];
 } else {
     // 將所選分類 ID 串接為 SQL 所用的字串
     $category_id_str = '';
 
-    foreach ($json_data as $category_id) {
+    foreach ($selected_categories as $category_id) {
         $category_id_str = $category_id_str . ',' . "'" . $category_id . "'";
     }
 
@@ -35,12 +33,14 @@ if (count($json_data) == 0) {
     // 執行：
     $sql_query_projects_list = "SELECT 
     PROJECT_ID, PROJECT_NAME, PROJECT_AVATAR_URL, PROJECT_SUMMARY, PROJECT_ORIGINAL_PRICE_PER_PERSON, PROJECT_MIN_NUM_OF_PEOPLE, 
-    FK_CATEGORY_ID_for_PJ FROM projects WHERE PROJECT_STATUS > 0 && FK_CATEGORY_ID_for_PJ IN " . $category_id_str;
+    FK_CATEGORY_ID_for_PJ FROM projects WHERE FK_CATEGORY_ID_for_PJ IN " . $category_id_str . " && PROJECT_ORIGINAL_PRICE_PER_PERSON <= ? 
+    && PROJECT_STATUS > 0 && PROJECT_VISIBLE_ON_WEB != 0 ORDER BY " . $sort;
     $statement_query_projects_list = $pdo->prepare($sql_query_projects_list);
+    $statement_query_projects_list->bindParam(1, $budget);
     $statement_query_projects_list->execute();
-}
 
-$result_query_projects_list = $statement_query_projects_list->fetchAll(PDO::FETCH_ASSOC);
+    $result_query_projects_list = $statement_query_projects_list->fetchAll(PDO::FETCH_ASSOC);
+}
 
 // 向前端回傳內容
 // $return_obj = (object)[
