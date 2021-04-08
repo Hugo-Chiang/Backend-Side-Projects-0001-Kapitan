9bf62c04973e1298f08264a189c4bb297c8c8777
Hugo-Chiang <statesman1991@gmail.com>
2021-04-09 02:32:01 +0800

    fix

--- Lib/CORS.php
+++ Lib/CORS.php
@@ -1,7 +1,7 @@
 <?php
 
-// header("Access-Control-Allow-Origin: http://localhost:8080");
-header("Access-Control-Allow-Origin: https://fe-sp-0001-kapitan.herokuapp.com");
+header("Access-Control-Allow-Origin: http://localhost:8080");
+// header("Access-Control-Allow-Origin: https://fe-sp-0001-kapitan.herokuapp.com");
 header("Content-Type:text/html; charset=utf-8");
 
 switch ($allow_methods) {
