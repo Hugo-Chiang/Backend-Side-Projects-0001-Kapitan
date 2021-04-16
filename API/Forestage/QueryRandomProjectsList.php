5d41678e385f11360e64742ffe2a69bada150c6b
Hugo-Chiang <statesman1991@gmail.com>
2021-04-16 15:30:33 +0800

    三階段測試

--- API/Forestage/QueryRandomProjectsList.php
+++ API/Forestage/QueryRandomProjectsList.php
@@ -10,7 +10,7 @@ include("../../Lib/PDO.php");
 // 執行：隨機挑選 6 組方案回傳予前端
 $sql_query_random_projects_list = "SELECT 
 PROJECT_ID, PROJECT_NAME, PROJECT_AVATAR_URL, PROJECT_SUMMARY, PROJECT_ORIGINAL_PRICE_PER_PERSON, PROJECT_MIN_NUM_OF_PEOPLE, PROJECT_MAX_NUM_OF_PEOPLE, 
-FK_CATEGORY_ID_for_PJ FROM projects WHERE PROJECT_VISIBLE_ON_WEB != 0 && PROJECT_FOR_TESTING != 1 && PROJECT_NAME NOT LIKE '卡位' ORDER BY RAND() LIMIT 6";
+FK_CATEGORY_ID_for_PJ FROM projects WHERE PROJECT_STATUS = 1 && PROJECT_VISIBLE_ON_WEB != 0 && PROJECT_FOR_TESTING != 1 ORDER BY RAND() LIMIT 6";
 $statement_query_random_projects_list = $pdo->prepare($sql_query_random_projects_list);
 $statement_query_random_projects_list->execute();
 
